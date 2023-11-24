242d0571ffaad857f811fb6a93ebfec953a07740
Dirk Arnez <smalldirkalex@gmail.com>
2023-11-24 15:14:17 +0800

    - add index.svelte

---
+++ index.svelte
@@ -0,0 +1,26 @@
+<script>
+
+
+		
+	let container;
+
+
+	
+	import {  onMount} from "svelte";
+
+	onMount(() => {
+var aa = eval(`var aa = function () {
+const button =  document.createElement("button");
+	button.innerHTML = "hi";
+	button.addEventListener("click", () => {alert()});
+	return button;
+};; (() => aa)()`);
+
+		
+		container.appendChild(aa());
+	});
+
+	
+</script>
+
+<div bind:this={container}></div>
